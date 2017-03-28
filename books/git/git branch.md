0dba33b0976b69c48ad9b5e3c1bff92dee911a16
moxingwang <xingwang.mo@chinaredstar.com>
2017-03-28 12:37:54 +0800

    first commit

---
+++ books/git/git branch.md
@@ -0,0 +1,4 @@
+* 删除本地分支 
+````
+git branch -D p-trade-oc-dev-show-20170314-presale
+````
---
+++ demo/src/main/java/mo/Main.java
@@ -0,0 +1,8 @@
+package main.java.mo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
