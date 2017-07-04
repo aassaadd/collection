963bafb7f4149be7314cd7f0466487c78c6b7e5d
xingwang.mo <xingwang.mo@chinaredstar.com>
2017-07-04 17:43:10 +0800

    commit

--- demo/src/main/java/mo/basic/Basic.java
+++ demo/src/main/java/mo/basic/Basic.java
@@ -1,15 +1,30 @@
 package main.java.mo.basic;
 
-import java.util.*;
+//import java.util.HashMap;
+
+import java.util.Set;
 
 /**
  * Created by MoXingwang on 2017/6/30.
  */
 public class Basic {
     public static void main(String[] args) {
-        HashSet<String> set = new HashSet<String>();
-        HashMap<String,String> hashMap = new HashMap<String, String>();
+        /*HashMap<String,String> hashMap = new HashMap<String, String>();
+        hashMap.put("a","11");
+        hashMap.get("a");
+        hashMap.put("b","11");
+        hashMap.put("c","11");
+        hashMap.put("d","11");*/
 
+        HashMap<Object,Object> hashMap = new HashMap<Object, Object>();
+        hashMap.put(new Object(),"11");
+        hashMap.put(new Object(),"11");
+        hashMap.put(new Object(),"11");
+        hashMap.put(new Object(),"11");
+        hashMap.get(new Object());
+
+        Set<Object> r = hashMap.keySet();
+        System.out.println("");
 
     }
 }
