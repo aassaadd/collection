78e7cff37ba0a3d992fb71baa91bf04a6d3312d2
xingwang.mo <xingwang.mo@chinaredstar.com>
2017-07-19 17:43:51 +0800

    commit

---
+++ demo/src/main/java/mo/multithread/volatiletest/VolatileTest.java
@@ -0,0 +1,18 @@
+package main.java.mo.multithread.volatiletest;
+
+/**
+ * Created by M on 17/7/19.
+ */
+public class VolatileTest {
+    private volatile Object object ;
+
+    public void testA(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("testA");
+    }
+
+
+}
