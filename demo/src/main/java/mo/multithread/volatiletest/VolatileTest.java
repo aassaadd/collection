49a63a9b36a80914807f80f8d8ec86ab0dfd9980
xingwang.mo <xingwang.mo@chinaredstar.com>
2017-07-22 17:22:39 +0800

    commit

--- demo/src/main/java/mo/multithread/volatiletest/VolatileTest.java
+++ demo/src/main/java/mo/multithread/volatiletest/VolatileTest.java
@@ -13,7 +13,6 @@ public class VolatileTest {
     public static void main(String[] args) {
         System.out.println("testA");
 
-
         try{
             throw new RuntimeException("test");
         }catch (Exception e){
