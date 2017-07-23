cc1684649336fa66d5b6922439452c13efd00c54
xingwang.mo <xingwang.mo@chinaredstar.com>
2017-07-23 21:07:34 +0800

    commit

---
+++ demo/src/main/java/mo/multithread/volatiletest/TestTwo.java
@@ -0,0 +1,38 @@
+package main.java.mo.multithread.volatiletest;
+
+public class TestTwo {
+
+     int i = 0;
+
+    public void add(){
+        System.out.println(i);
+        i ++;
+    }
+
+    public void testA(){
+        while (true){
+            if(i > 0){
+                System.out.println("------------------i > 0");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TestTwo testTwo = new TestTwo();
+        Thread thread1 = new Thread(() -> {
+            testTwo.testA();
+        });
+        thread1.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while (testTwo.i < 10000){
+            testTwo.add();
+        }
+    }
+}
