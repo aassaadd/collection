f35934f1336ffc6f0ade8067fe8928050c846787
xingwang.mo <xingwang.mo@chinaredstar.com>
2017-07-23 22:21:36 +0800

    commit

--- demo/src/main/java/mo/multithread/volatiletest/TestTwo.java
+++ demo/src/main/java/mo/multithread/volatiletest/TestTwo.java
@@ -1,16 +1,38 @@
 package main.java.mo.multithread.volatiletest;
 
+/**
+ * 缓存行的原因
+ * 差点思考城傻逼了
+ */
+
 public class TestTwo {
 
      int i = 0;
+    volatile int b = 0;
 
     public void add(){
         System.out.println(i);
         i ++;
+        b ++;
     }
 
     public void testA(){
         while (true){
+//            Integer.toString(i);
+
+//            toString(i);
+
+//            String s = new String("11212");
+//            Object a = new Integer(10);
+
+//            String aaa = "dsfsdfsd";
+
+//                new Object();
+
+//            if(b > 0){
+                System.out.println("b " + b);
+//            }
+
             if(i > 0){
                 System.out.println("------------------i > 0");
                 break;
@@ -35,4 +57,16 @@ public class TestTwo {
             testTwo.add();
         }
     }
+
+//    public static String toString(int i) {
+//        if (i == Integer.MIN_VALUE){
+//            return "-2147483648";
+//
+//        }
+//        char[] buf = new char[10];
+//        return new String("qq");
+//    }
+
+
+
 }
