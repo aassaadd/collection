c56e31a2e43e37ce00ecd6d9ee4a5966e298fc9c
xingwang.mo <xingwang.mo@chinaredstar.com>
2017-07-23 20:09:50 +0800

    commit

--- demo/src/main/java/mo/multithread/volatiletest/Bar.java
+++ demo/src/main/java/mo/multithread/volatiletest/Bar.java
@@ -5,4 +5,14 @@ package main.java.mo.multithread.volatiletest;
  */
 public class Bar {
 
+    private int id = 0;
+
+    public void test(){
+        id ++;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+
 }
