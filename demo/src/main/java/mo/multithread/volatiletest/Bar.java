1be46e3272fdbc51bbcf8a0987ad7194e97e8c84
xingwang.mo <xingwang.mo@chinaredstar.com>
2017-07-27 12:48:07 +0800

    commit

--- demo/src/main/java/mo/multithread/volatiletest/Bar.java
+++ demo/src/main/java/mo/multithread/volatiletest/Bar.java
@@ -3,6 +3,9 @@ package main.java.mo.multithread.volatiletest;
 /**
  * Created by M on 17/7/23.
  * java -XX:+PrintAssembly -Xcomp -XX:CompileCommand=dontinline,*Bar.sum -XX:CompileCommand=compileonly,*Bar.sum Bar
+ *
+ * export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/bin/javac
+
  */
 public class Bar {
 
